a644b1604cc0b2c0aa286e97a7b9fed6659d5a0a
Frederik Baymler Mathiesen <frederik@baymler.com>
2022-11-01 15:50:53 +0100

    Bulk load linear

--- src/linear/storage.jl
+++ src/linear/storage.jl
@@ -4,6 +4,11 @@ mutable struct LinearIndex{T, E, VE<:AbstractVector{E}} <: AbstractSpatialIndex{
     container::VE
 end
 LinearIndex{T}(container::VE) where {T, VE<:AbstractVector} = LinearIndex{T, eltype(VE), VE}(container)
+LinearIndex{T, E}() where {T, E} = LinearIndex{T, E, Vector{E}}(Vector{E}())
+
+function bulk_load!(index::LinearIndex{T, E, VE}, data::VE) where {T, E, VE<:AbstractVector{E}}
+    index.container = copy(data)
+end
 
 Base.isempty(index::LinearIndex) = isempty(index.container)
 Base.length(index::LinearIndex) = length(index.container)
